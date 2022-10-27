23ab2a461d962b189ca099ff94fd60f98e57f352
ibrahim-syah <ibrahim.syah.q@gmail.com>
2022-10-27 19:01:49 +0700

    changed hardcoded file separator for unix compatibility

--- src/main/java/org/example/helper/MessageTemplate.java
+++ src/main/java/org/example/helper/MessageTemplate.java
@@ -1,15 +1,12 @@
 package org.example.helper;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class MessageTemplate {
     public static ArrayList<String> readData() throws IOException {
         String filename = "message_template.txt";
-        String pathToFile = System.getProperty("user.dir") + "\\" + filename;
+        String pathToFile = System.getProperty("user.dir") + File.separator + filename;
         ArrayList<String> content = new ArrayList<>();
         content.add("Halo ");
         try(BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
--- src/main/java/org/example/helper/RecipientsLoader.java
+++ src/main/java/org/example/helper/RecipientsLoader.java
@@ -1,16 +1,13 @@
 package org.example.helper;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecipientsLoader {
     public static List<String[]> readData() throws IOException {
         String file = "recipients.csv";
-        String pathToFile = System.getProperty("user.dir") + "\\" + file;
+        String pathToFile = System.getProperty("user.dir") + File.separator + file;
         List<String[]> content = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
             String line;
